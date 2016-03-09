d8d893322c688bdd9330820821f97a15aa752b03
Gaston Briones <mauricio_briones@artear.com.ar>
2016-03-09 17:46:55 -0300

    fix

--- wp-content/themes/inaki/content-header.php
+++ wp-content/themes/inaki/content-header.php
@@ -1,3 +1,14 @@
-<header id="header" class="block wrapper clearfix">
-
+<header id="header" class="clearfix">
+  <div class="col ">
+    <ul class="social">
+      <li class="oleo"></li>
+      <li class="tripadvisor"></li>
+      <li class="google"></li>
+      <li class="facebook"></li>
+    </ul>
+  </div>
+  <div class="col">
+    <div>logo</div>
+  </div>
+  <div>
 </header>
--- wp-content/themes/inaki/index.php
+++ wp-content/themes/inaki/index.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 <body <?php body_class(); ?>>
 <div id="site">
+  <div class="wrapper clearfix">
 
+  </div>
 </div>
 </body>
 </html>
