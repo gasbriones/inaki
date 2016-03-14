abdaf1e739165e8bef5c68e2ab4917e5c08c5e43
Gaston Briones <mauricio_briones@artear.com.ar>
2016-03-14 16:24:38 -0300

    home complete

--- wp-content/themes/inaki/content-header.php
+++ wp-content/themes/inaki/content-header.php
@@ -1,14 +1,15 @@
 <header id="header" class="clearfix">
   <div class="grid-reverse">
-    <div class="col-3_lg-5_xs-12">
-      <div class="menu left">
-        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/mobile-menu.png" alt="Iñaki mobile menu"/></a>
-      </div>
-      <div class="logo right">
+    <div class="col-3_lg-4_md-4_sm-5_xs-12">
+      <div class="logo left">
         <a href="<?php echo get_site_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="Iñaki restaurante logo"></a>
       </div>
+      <div class="menu right">
+        <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/mobile-menu.png" alt="Iñaki mobile menu"/></a>
+      </div>
+
     </div>
-    <div class="col-9_lg-7_xs-12">
+    <div class="col-9_lg-8_md-8_sm-7_xs-12">
       <ul class="social clearfix">
         <li><a href="#" class="icon oleo"></a></li>
         <li><a href="#" class="icon tripadvisor"></a></li>
--- wp-content/themes/inaki/content-main.php
+++ wp-content/themes/inaki/content-main.php
@@ -1,7 +1,9 @@
 <section id="main" class="clearfix">
   <div class="grid-3-center">
-    <div class="col">
-      imagen home
+    <div class="col-6_lg-7_md-7_sm-10_xs-12">
+      <a href="#">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/plato-rojo.png" alt="Iñaki enter site" class="enter-site-img"/>
+      </a>
     </div>
   </div>
 </section>
