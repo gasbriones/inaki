ddb705989d319ca81dd67ebb1c3f4980c0497172
Gaston Briones <mauricio_briones@artear.com.ar>
2016-03-15 18:01:57 -0300

    add img legend

--- wp-content/themes/inaki/la-historia-tpl.php
+++ wp-content/themes/inaki/la-historia-tpl.php
@@ -7,24 +7,26 @@ get_header(); ?>
 <div class="wrapper clearfix">
     <div class="grid site">
         <div class="col-7 content-wrapper">
-            <h2><?php the_title(); ?></h2>
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <article>
-                    <?php the_content(); ?>
-                </article>
-                <div class="home-img">
-                    <img src="<?php echo the_field('imagen_1_la_historia') ?>"/>
-                </div>
-                <article>
-                    <?php echo the_field('bloque_2_la_historia') ?>
-                </article>
-                <div class="home-img">
-                    <img src="<?php echo the_field('imagen_2_la_historia') ?>"/>
-                </div>
-                <article>
-                    <?php echo the_field('bloque_3_la_historia') ?>
-                </article>
-            <?php endwhile; endif; ?>
+            <h2 class="page-title"><?php the_title(); ?></h2>
+            <section class="clearfix">
+                <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <article class="paragraph">
+                        <?php the_content(); ?>
+                    </article>
+                    <div class="section-img">
+                        <img src="<?php echo the_field('imagen_1_la_historia') ?>"/>
+                    </div>
+                    <article class="paragraph">
+                        <?php echo the_field('bloque_2_la_historia') ?>
+                    </article>
+                    <div class="section-img">
+                        <img src="<?php echo the_field('imagen_2_la_historia') ?>"/>
+                    </div>
+                    <article class="paragraph">
+                        <?php echo the_field('bloque_3_la_historia') ?>
+                    </article>
+                <?php endwhile; endif; ?>
+            </section>
         </div>
         <div class="col-5 menu-wrapper">
             <?php get_template_part('menu'); ?>
