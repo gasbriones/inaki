70c178e413a94e4116762405bc43ed072c39c939
Gaston Briones <mauricio_briones@artear.com.ar>
2016-03-18 16:51:57 -0300

    fix

--- wp-content/themes/inaki/menu-tpl.php
+++ wp-content/themes/inaki/menu-tpl.php
@@ -1,13 +1,16 @@
-<nav class="mobile-menu-wrapper">
-    <div class="grid-3-center buttons-nav">
-        <div class="col-10_lg-9_md-11_sm-12_xs-12">
-            <div class="close left">
-                X Cerrar
-            </div>
-            <div class="logo left">
-                <a href="<?php echo get_site_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo-white.png" alt="Iñaki restaurante logo"></a>
+<div class="grid mobile-menu-wrapper">
+    <div class="col-10_lg-12_md-12_sm-11_xs-12">
+        <div class="grid-3-center buttons-nav">
+            <div class="col-10_lg-9_md-11_sm-11_xs-12">
+                <div class="close left">
+                    X Cerrar
+                </div>
+                <div class="logo left">
+                    <a href="<?php echo get_site_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo-white.png" alt="Iñaki restaurante logo"></a>
+                </div>
             </div>
         </div>
+        <?php wp_nav_menu(array('theme_location' => 'mobile-menu')); ?>
     </div>
-    <?php wp_nav_menu(array('theme_location' => 'mobile-menu')); ?>
-</nav>
+</div>
+
